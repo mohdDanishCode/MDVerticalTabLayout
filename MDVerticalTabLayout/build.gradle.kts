44e988df22505d2ffaa0b45f8766d1421c1b6aef
mddanish <mddanish532@gmail.com>
2023-10-26 20:37:16 +0530

    feat: change java version to 17

--- MDVerticalTabLayout/build.gradle.kts
+++ MDVerticalTabLayout/build.gradle.kts
@@ -31,11 +31,11 @@ android {
         kotlinCompilerExtensionVersion = "1.4.3"
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
